fc5c3aafeaaed99647dd35f1133f7ec17535d33e
intrnl <intrnl28+git@gmail.com>
2022-03-05 13:25:30 +0700

    refactor!: properly resolve css modules imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
+
 import * as esbuild from 'esbuild';
 import postcss from 'postcss';
 import postcssModules from 'postcss-modules';
@@ -12,7 +13,7 @@ const RE_CSS = /.\.css$/i;
 const RE_OUTPUT = /.\.css\?__postcss$/i;
 const RE_MODULE = /.\.module\.[a-z]+$/i;
 
-const VERSION = 1;
+const VERSION = 2;
 
 /**
  * @param {object} options
@@ -21,21 +22,9 @@ const VERSION = 1;
 export default function postcssPlugin (options = {}) {
 	const { modules = true, cache = true, plugins = [] } = options;
 
-	const modulePlugin = modules && postcssModules({
-		scopeBehaviour: 'local',
-		localsConvention: 'camelCaseOnly',
-		generateScopedName: '[local]_[hash:6]',
-		getJSON () {},
-		...(modules === true ? {} : modules),
-	});
-
 	return {
-		name: 'postcss',
+		name: '@intrnl/esbuild-plugin-postcss',
 		async setup (build) {
-			if (build.__resolve) {
-				return;
-			}
-
 			const fsCache = cache && new FSCache({
 				...await getProjectRoot('@intrnl/esbuild-plugin-postcss'),
 			});
@@ -53,7 +42,7 @@ export default function postcssPlugin (options = {}) {
 
 				const key = [
 					VERSION,
-					isModule && modules,
+					isModule,
 					plugins.map((plugin) => plugin?.postcssPlugin),
 				];
 
@@ -98,48 +87,75 @@ export default function postcssPlugin (options = {}) {
 					namespace: 'postcss',
 				};
 			});
+
+			async function loader (filename, isModule) {
+				const source = await fs.readFile(filename, 'utf-8');
+
+				const processorPlugins = plugins;
+
+				if (isModule) {
+					const mod = postcssModules({
+						scopeBehavior: 'local',
+						localsConvention: 'camelCaseOnly',
+						generateScopedName: '[local]_[hash:6]',
+						getJSON () {},
+						async resolve (id) {
+							const result = await build.resolve(id, {
+								importer: filename,
+								resolveDir: path.dirname(filename),
+							});
+
+							if (result.errors.length > 0) {
+								return id;
+							}
+
+							return result.path;
+						},
+						...(modules === true ? null : modules),
+					});
+
+					processorPlugins = plugins.slice();
+					processorPlugins.unshift(mod);
+				}
+
+				const processor = postcss(processorPlugins);
+				const result = await processor.process(source, { from: filename });
+
+				// Retrieve module exports for CSS modules and any file dependencies
+				const dependencies = [];
+				let moduleObj;
+
+				for (const message of result.messages) {
+					if (message.type === 'dependency') {
+						dependencies.push(message.file);
+					}
+					else if (message.type === 'export' && message.plugin === 'postcss-modules') {
+						moduleObj = message.exportTokens;
+					}
+				}
+
+				// Retrieve build warnings
+				const warnings = [];
+
+				for (const message of result.warnings()) {
+					warnings.push({
+						text: message.text,
+						location: {
+							line: message.line,
+							column: message.column,
+						}
+					})
+				}
+
+				// Resulting output
+				const css = result.css;
+				const js = `
+					import ${JSON.stringify(path.basename(filename) + '?__postcss')};
+					${dataToEsm(moduleObj || {})}
+				`;
+
+				return { dependencies, warnings, css, js };
+			}
 		},
 	};
-
-	async function loader (filename, isModule) {
-		const source = await fs.readFile(filename, 'utf-8');
-
-		const processor = postcss(isModule ? [modulePlugin, ...plugins] : plugins);
-		const result = await processor.process(source, { from: filename });
-
-		// Retrieve module exports for CSS modules and any file dependencies
-		const dependencies = [];
-		let moduleObj;
-
-		for (const message of result.messages) {
-			if (message.type === 'dependency') {
-				dependencies.push(message.file);
-			}
-			else if (message.type === 'export' && message.plugin === 'postcss-modules') {
-				moduleObj = message.exportTokens;
-			}
-		}
-
-		// Retrieve build warnings
-		const warnings = [];
-
-		for (const message of result.warnings()) {
-			warnings.push({
-				text: message.text,
-				location: {
-					line: message.line,
-					column: message.column,
-				}
-			})
-		}
-
-		// Resulting output
-		const css = result.css;
-		const js = `
-			import ${JSON.stringify(path.basename(filename) + '?__postcss')};
-			${dataToEsm(moduleObj || {})}
-		`;
-
-		return { dependencies, warnings, css, js };
-	}
 }
